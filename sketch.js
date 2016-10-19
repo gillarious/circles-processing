62d22d5fbc99701a263d99d4c08c8e9748f66370
gillarious <isabella.berryx3@gmail.com>
2016-10-19 15:50:11 -0400

    lmao
    lmao

--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ function setup() {
 
 function draw() {
 	background(255);
-	currentCircles = new Circle(this, mouseX, mouseY);
 	if (mouseIsPressed) {
+		currentCircles = new Circle(this, mouseX, mouseY);
 		theCircles.push(currentCircles);
 		currentCircles++;
 		if (numCircles < theCircles.length){
