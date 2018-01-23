82eb84fa6c0d797be86d7619b90491b9692cfb6a
gillarious <isabella.berryx3@gmail.com>
2018-01-23 04:08:03 -0500

    lmao
    lmao

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
-//var theCircles = [];
-var circle1;
+var circles = [];
 
 function setup() {
 	var canvas = createCanvas(window.innerWidth, window.innerHeight);
@@ -7,20 +6,16 @@ function setup() {
 	smooth();
 }
 
+function mousePressed() {
+	circles.push(new Circle(mouseX, mouseY));
+}
+
 function draw() {
 	background(255,255,255);
-	if (mouseIsPressed) {
-		//theCircles[new Circle(this, mouseX, mouseY)];
-		circle1 = new Circle(this, mouseX, mouseY);
-		circle1.display();
-		circle1.move();
-		//circle1.fade();
+	for (var i = 0; i < circles.length; i++) {
+		circles[i].update();
+		circles[i].show();
 	}
-	/*for (var i = 0; i < theCircles.length; i++) {
-		theCircles[i].fade();
-		theCircles[i].move();
-		theCircles[i].display();
-	}*/
 }
 
 window.onresize = function() {
@@ -30,63 +25,3 @@ window.onresize = function() {
 	width = w;
 	height = h;
 };
-
-function Circle(canvas, x, y){
-	var x;
-	var y;
-	var size;
-	var myRed;
-	var myGreen;
-	var myBlue;
-	var myAlpha;
-	var speedX;
-	var speedY;
-
-	this.canvas = canvas;
-	this.x = x;
-	this.y = y;
-	size = this.canvas.random(15, 35);
-	myRed = 255;
-	myGreen = this.canvas.random(154, 229);
-	myBlue = 235;
-	myAlpha = this.canvas.random(0, 255);
-	speedX = this.canvas.random(-5, 5);
-	speedY = this.canvas.random(-5, 5);
-
-	Circle.prototype.move = function() {
-		x += speedX;
-		y += speedY;
-
-	  	if (x > width) {
-			x = width;
-			speedX *= -1;
-	    }
-		if (y > height) {
-			y = height;
-			speedY *= -1;
-		}
-		if (x < 0) {
-			x =0;
-			speedX *= -1;
-		}
-		if (y < 0) {
-			y = 0;
-			speedY *= -1;
-		}
-	}
-
-	Circle.prototype.display = function() {
-		this.canvas.noStroke();
-		this.canvas.fill(myRed, myGreen, myBlue, myAlpha);
-		this.canvas.ellipse(x, y, size, size);
-	}
-
-	Circle.prototype.fade = function() {
-		if (myAlpha > 0) {
-			myAlpha -= 3;
-		}
-		else {
-			myAlpha = 0;
-		}
-	}
-}
